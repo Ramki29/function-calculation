2f89f40c05c007d750577529f9a80ae8756db236
Ramki29 <ramkie7010@gmail.com>
2023-03-24 21:48:45 +0530

    add function-caculation

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function calculate(b,c)
+	{
+	let add=b+c;
+	document.write(add);
+	document.write("<br>");
+	let sub=b-c;
+	document.write(sub);
+	document.write("<br>");
+	let multi=b*c;
+	document.write(multi);
+	document.write("<br>");
+	let div=b/c;
+	document.write(div);
+	document.write("<br>");
+	let modulo=b%c;
+	document.write(modulo);
+	document.write("<br>");
+	}
+	calculate(20,10);
+	
+	
+function cal(x,y)
+	{
+	let add=x+y;
+	return add;
+	}
+	let result=cal(5,10);
+	document.write(result*2);
